d9506bbcbacc7885aa3ca3ca49a47fe1c7234184
Nazmul Hasan Robin <robin.sust08@gmail.com>
2024-01-19 18:12:57 +0600

    built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nhrrob/nhrrob-popular-plugins',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7a9f9d080c026e4da036a944916b1bcfadd00164',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nhrrob/nhrrob-popular-plugins' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7a9f9d080c026e4da036a944916b1bcfadd00164',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
